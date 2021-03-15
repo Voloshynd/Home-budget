70324af1dcc812a7b60bb684d35b9311a0814901
Dmytro Voloshyn <voloshynd13@gmail.com>
2021-03-15 09:35:37 +0100

    Przelicznik_walut

---
+++ script/index.js
@@ -0,0 +1,54 @@
+const input = document.getElementById("num");
+const button = document.getElementById("equal");
+const select = document.getElementById("select-option");
+const total = document.getElementById("total");
+
+const currency= {
+    EUR: "",
+    USD: "",
+    CHF: ""
+}
+axios("http://api.nbp.pl/api/exchangerates/rates/a/usd")
+.then((res) => {
+    currency.USD = res.data.rates[0].mid
+        
+})
+.catch((error) =>{
+    alert(error)
+})
+axios("http://api.nbp.pl/api/exchangerates/rates/a/chf/")
+.then((res) => {
+    currency.CHF = res.data.rates[0].mid     
+})
+.catch((error) =>{
+    alert(error)
+})
+axios("http://api.nbp.pl/api/exchangerates/rates/a/eur/")
+.then((res) => {
+    currency.EUR = res.data.rates[0].mid   
+})
+.catch((error) =>{
+    alert(error)
+})
+
+button.addEventListener("click", ()=>{
+    let valueSelect = select.value;
+    let valueInput = Number(input.value);
+    if(valueSelect === "EUR"){
+        total.textContent = "TO " + Math.round(valueInput * currency.EUR) + " PLN";
+    } else if (valueSelect ==="USD"){
+        total.textContent = "TO " + Math.round(valueInput * currency.USD) + " PLN";
+    }else if (valueSelect ==="CHF"){
+        total.textContent = "TO " + Math.round(valueInput *  currency.CHF) + " PLN";
+    }  
+    // else (valueInput === 0) {
+    //     input.style.borderColor ="red";
+    // } 
+
+})
+
+
+
+
+   
+    
